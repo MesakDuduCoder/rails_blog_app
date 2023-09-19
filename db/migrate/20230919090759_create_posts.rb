8e16994a8e17d66a6e209fdbc7ddaba672dc5451
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-19 22:17:45 +0530

    Add migration

--- db/migrate/20230919090759_create_posts.rb
+++ db/migrate/20230919090759_create_posts.rb
@@ -1,12 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      author_id :integer
-      title :string
-      text :text
+      t.string :title 
+      t.text :text
       t.timestamps
-      comments_counter: integer
-      likes_counter: integer
+      t.integer :comments_counter
+      t.integer :likes_counter 
     end
   end
 end
