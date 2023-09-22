272e87d7a9a25ff419963d801f5bbdb3ab028aa7
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-22 17:44:44 +0530

    Add user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'should return recent posts' do
+    user = FactoryBot.create(:user)
+    FactoryBot.create_list(:post, 5, author: user)
+
+    recent_posts = user.recent_posts
+
+    expect(recent_posts.length).to eq(3)
+    expect(recent_posts).to eq(user.posts.order(created_at: :desc).limit(3))
+  end
+
+  it 'should validate presence of :name' do
+    user = User.new
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it 'should validate_numericality_of :posts_counter it is only_integer and is_greater_than_or_equal_to(0)' do
+    user = User.new
+    user.valid?
+    expect(user.errors[:posts_counter]).to include('is not a number')
+  end
+end
