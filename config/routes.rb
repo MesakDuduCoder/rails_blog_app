475a81388ca9c162d78121779898878e98242507
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-25 18:45:24 +0530

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
-    # Nested route for posts under a specific user
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] 
   end
-
-  # Defines the root path route ("/")
   root "users#index"
 end
