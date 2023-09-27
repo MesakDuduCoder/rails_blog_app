ce0318aaf90753cc9db382a53f5e4ad6a9855a5d
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-27 15:45:25 +0530

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
-  def index; end
+  before_action :set_user
+  before_action :set_post, only: [:show]
+
+  def index
+    @posts = @user.posts
+  end
 
   def show; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.find(params[:id])
+  end
 end
