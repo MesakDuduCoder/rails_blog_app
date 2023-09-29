447673ae6e240f6664297d132f05f170a9a95373
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-30 02:16:39 +0530

    Add create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,23 @@ class PostsController < ApplicationController
 
   def show; end
 
+  def create
+    puts 'create method is being called!'
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post created successfully.'
+    else
+      flash[:error] = @post.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
   private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 
   def set_user
     @user = User.find(params[:user_id])
