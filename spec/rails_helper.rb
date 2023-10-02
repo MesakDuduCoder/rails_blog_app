7b40f6ebd38dda5e8ff89bb36caf0554451528d9
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-02 23:56:58 +0530

    Update rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,9 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-# Prevent database truncation if the environment is production
+require 'capybara/rspec'
+Capybara.javascript_driver = :selenium_chrome_headless
+
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 RSpec.configure do |config|
