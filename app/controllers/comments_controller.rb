fb240df15e6d918809ec3f73ad31540d0ea69dcc
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-03 00:34:03 +0530

    Update controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :set_user
   def create
     post_id = params[:post_id]
+    user_id = params[:user_id]
+
     @comment = Comment.new(user_id: current_user.id, post_id:, text: comment_params[:text])
     puts @comment.inspect
     if @comment.save
-      puts 'Comment saved successfully!'
-      redirect_to user_post_path(current_user.id, post_id), notice: 'Comment created successfully.'
+      redirect_to user_post_path(user_id:, id: post_id), notice: 'Comment created successfully.'
     else
       render :new
     end
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:text)
