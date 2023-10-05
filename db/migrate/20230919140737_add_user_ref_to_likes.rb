9bb12f7d95d4cd1c1a99cfa633e4202ebd60404f
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-06 02:20:31 +0530

    Update migration for cascade delete

--- db/migrate/20230919140737_add_user_ref_to_likes.rb
+++ db/migrate/20230919140737_add_user_ref_to_likes.rb
@@ -1,6 +1,6 @@
 class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :user, foreign_key: true
-    add_reference :likes, :post, foreign_key: true
+    add_reference :likes, :user, foreign_key: { on_delete: :cascade }
+    add_reference :likes, :post, foreign_key: { on_delete: :cascade }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -65,9 +65,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_080205) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
+  add_foreign_key "comments", "posts", on_delete: :cascade
+  add_foreign_key "comments", "users", on_delete: :cascade
+  add_foreign_key "likes", "posts", on_delete: :cascade
+  add_foreign_key "likes", "users", on_delete: :cascade
   add_foreign_key "posts", "users", column: "author_id"
 end
