08fb3f85688fcfada5c9d60d5db97c81f2b65339
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-06 16:03:28 +0530

    Update

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new 
 
-    can :destroy, Post, author_id: user.id
-    can :destroy, Comment, user_id: user.id
-
-    can :destroy, Post if user.role == 'admin'
-    can :destroy, Comment if user.role == 'admin'
   end
 end
