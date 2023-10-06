da15a002e82a06e50d8a9e2d38ffeed30ed7b49d
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-06 22:37:06 +0530

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,16 @@ Rails.application.routes.draw do
       resources :likes
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :posts do
+         resources :comments, only: [:create]
+        end
+      end
+    end
+  end
+
   root to: "users#index"
 end
