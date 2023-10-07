f1f2319ab259a221bcc7ac3ddfb945f2588a40c1
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-07 09:33:52 +0530

    Set comment creator

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,24 +1,28 @@
 class Api::V1::CommentsController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    skip_before_action :authenticate_user!
-    before_action :find_user
-    before_action :find_post
-    def create
-      @comment = @post.comments.build(user: @user, text: comment_params[:text])
-      if @comment.save
-        render json: @comment, status: :created
-      else
-        render json: @comment.errors, status: :unprocessable_entity
-      end
-    end
-    private
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
-    def find_post
-      @post = @user.posts.find(params[:post_id])
-    end
-    def find_user
-      @user = User.find(params[:user_id])
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
+  before_action :find_user
+  before_action :find_post
+  def create
+    @comment = @post.comments.build(user: current_user, text: comment_params[:text])
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def find_post
+    @post = @user.posts.find(params[:post_id])
+  end
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+end
